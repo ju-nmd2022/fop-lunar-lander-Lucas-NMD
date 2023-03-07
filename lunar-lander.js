b17a6e34d8e173d57f225bd46b1bb966d0bf3d09
Lucas-NMD <117161421+Lucas-NMD@users.noreply.github.com>
2023-03-07 19:42:20 +0100

    d

--- lunar-lander.js
+++ lunar-lander.js
@@ -1,10 +1,5 @@
 let rocketImg;
-let moonSurface = {
-  x: width / 2,
-  y: height,
-  r: width,
-  h: 100
-};
+
 function preload() {
   rocketImg = loadImage('rocket.png');
 }
@@ -29,55 +24,70 @@ const keys = {
 };
 
 function draw() {
-  if (!isRunning) {
-  // Start screen
-    background(bg);
-    fill(255);
-    textSize(48);
-    textAlign(CENTER);
-    text('Lunar Landing', width/2, height/2 - 40);
-    textSize(24);
-    text('Use space or down arrow to control thrust', width/2, height/2 + 20);
-    textSize(32);
-    text('Click to start', width/2, height/2 + 80);
-  } else {
-    // Game screen
-    background(bg);
-    fill(255);
-    textSize(16); 
-    textAlign(RIGHT);
-    text('Velocity: ' + velocity.toFixed(2) + 'm/s', width - 20, 50);
-    // Draw moon surface
-    fill('#cccccc');
-    ellipseMode(CENTER);
-    ellipse(moonSurface.x, moonSurface.y, moonSurface.r, moonSurface.h);
-    // Update spaceship position
-    rocket (350, rocketY);
-    rocketY += velocity;
-    velocity += gravity;
-    // Check for landing or crash
-    if (rrocketY + 30 >= moonSurface) {
-      if (velocity <= 3) {
-        isLanded = true;
-        isGameOver = true;
-      } else {
-        isGameOver = true;
-      }
-    }
-   
-    // Game over logic
-    if (isGameOver) {
-      noLoop();
+    if (!isRunning) {
+      // Start screen
+      background(bg);
+      fill(255);
       textSize(48);
       textAlign(CENTER);
-      fill(255, 0, 0);
-      text(isLanded? 'Congratulations! You landed safely.' : 'Oops! You crashed.', width/2, height/2);
+      text('Lunar Landing', width/2, height/2 - 40);
+      textSize(24);
+      text('Use space or down arrow to control thrust', width/2, height/2 + 20);
       textSize(32);
-      text('Click to play again', width/2, height/2 + 60);
+      text('Click to start', width/2, height/2 + 80);
+    } else {
+      // Game screen
+      background(bg);
+      fill(255);
+      textSize(16); 
+      textAlign(RIGHT);
+      text('Velocity: ' + velocity.toFixed(2) + 'm/s', width - 20, 50);
+      // Update spaceship position
+      rocket (350, rocketY);
+      rocketY += velocity;
+      velocity += gravity;
+      // Check for landing or crash
+      if (rocketY + 30 >= 500) {
+        if (velocity <= 2) {
+          isLanded = true;
+          isGameOver = true;
+        } else {
+          isGameOver = true;
+        }
+      }
+      // Draw moon surface
+    fill('#cccccc');
+    ellipseMode(CENTER);
+    ellipse(width/2, height, width, 100);
+      // Game over logic
+      if (isGameOver) {
+        noLoop();
+        textSize(48);
+        textAlign(CENTER);
+        fill(255, 0, 0);
+        text(isLanded? 'Congratulations! You landed safely.' : 'Oops! You crashed.', width/2, height/2);
+        textSize(32);
+        text('Click to play again', width/2, height/2 + 60);
+      }
     }
   }
-}
   
+  /* window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case 'ArrowDown':
+        velocity = 1;
+        break;
+    }
+  });
+
+  window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+      case 'ArrowDown':
+        velocity = 0;
+        break;
+    }
+  }); */
+
   // Keyboard controls
 function keyPressed() {
   if (keyCode === 32 || keyCode === DOWN_ARROW) {
